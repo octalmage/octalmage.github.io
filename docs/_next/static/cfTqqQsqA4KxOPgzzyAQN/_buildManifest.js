f00b24afee76fec963e37e440730ea05f77f04a2
Jason Stallings <jason@stallin.gs>
2023-03-20 23:04:54 -0500

    fix: Use static export.

---
+++ docs/_next/static/cfTqqQsqA4KxOPgzzyAQN/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST=function(e){return{__rewrites:{beforeFiles:[],afterFiles:[{source:"/(links|lnk|l)",destination:e}],fallback:[]},"/":["static/chunks/61-ee77a64b410ab641.js","static/chunks/pages/index-1a1da79688ccf7ec.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:[e,"/_app","/_error"]}}("/"),self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    distDir: 'docs',
     swcMinify: true,
+    images: {
+        unoptimized: true,
+    },
     compiler: {
         // ssr and displayName are configured by default
         styledComponents: true,
